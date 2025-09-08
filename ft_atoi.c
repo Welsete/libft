14289752b17615e4df72bb59dbd74267a5537528
Wellerson <wtavares@student.42sp.org.br>
2025-09-08 14:41:36 -0300

    Atoi enxuto

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,33 +6,31 @@
 /*   By: wtavares <wtavares@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/03 22:16:58 by wtavares          #+#    #+#             */
-/*   Updated: 2025/09/03 22:17:12 by wtavares         ###   ########.fr       */
+/*   Updated: 2025/09/08 14:41:10 by wtavares         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_atoi(const char *nptr)
+int	ft_atoi(const char *str)
 {
-	int		i;
-	int		sign;
-	long	sum;
+	int	sign;
+	int	result;
 
-	i = 0;
 	sign = 1;
-	sum = 0;
-	while (nptr[i] == 32 || (nptr[i] >= 9 && nptr[i] <= 13))
-		i++;
-	if (nptr[i] == '-' || nptr[i] == '+')
+	result = 0;
+	while ((*str == 32) || (*str >= 9 && *str <= 13))
+		str++;
+	if (*str == '-' || *str == '+')
 	{
-		if (nptr[i] == '-')
+		if (*str == '-')
 			sign = -1;
-		i++;
+		str++;
 	}
-	while (nptr[i] >= '0' && nptr[i] <= '9')
+	while (*str >= '0' && *str <= '9')
 	{
-		sum = (sum * 10) + (nptr[i] - '0');
-		i++;
+		result = result * 10 + (*str - '0');
+		str++;
 	}
-	return (sum * sign);
+	return (result * sign);
 }
